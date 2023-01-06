169c41dd7d113f2cc19b3e3c90bc3c365fe92310
KevZ1209 <kevinbowditch11@gmail.com>
2023-01-06 15:06:05 -0800

    add intro animations

--- js/index.js
+++ js/index.js
@@ -1,4 +1,44 @@
 
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    $("#big-name").animate({
+        opacity: '1',
+        bottom: '0'
+    }, 2000)
+
+    $("#profile-img").animate({
+        opacity: '1',
+        bottom: '0'
+    }, 2000)
+
+    setTimeout(function() {
+        $("#tagline1").animate({
+            opacity: '1',
+            top: '0'
+        }, 2000)
+    }, 750)
+
+
+    setTimeout(function() {
+        $("#tagline2").animate({
+            opacity: '1',
+            top: '0'
+        }, 2000)
+    }, 3000)
+
+    setTimeout(function() {
+        $("#tagline3").animate({
+            opacity: '1',
+            top: '0'
+        }, 2000)
+    }, 5250)
+
+
+
+});
+
+
 function toggleNav() {
     $(".links").toggleClass('hide')
 
